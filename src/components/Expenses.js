83c445dd682522ccaaa7c0b61c5953cecf7b8145
=Ethan Harvey <sgwethan@outlook.com>
2022-01-11 22:48:04 +0000

    changed expenses

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,8 +1,6 @@
 import ExpenseItem from "./ExpenseItem";
 
 function Expenses(props) {
-  console.log(props)
-  console.log(props[0])
   return <ExpenseItem title={props.title} amount={props.amount} date={props.date}></ExpenseItem>
 }
 
