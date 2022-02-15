6c895deb80a9b4494cb70a90dd0ae8817978793b
=Ethan Harvey <sgwethan@outlook.com>
2022-02-15 15:47:40 +0000

    Rendering conditional content

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,7 +4,7 @@ import { useState } from "react";
 import ExpensesFilter from "./ExpenseFilter";
 
 function Expenses(props) {
-  const [filteredYear, setFilteredYear] = useState('2021');
+  const [filteredYear, setFilteredYear] = useState("2021");
 
   const filteredExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() == filteredYear;
@@ -14,18 +14,23 @@ function Expenses(props) {
     setFilteredYear(selectedYear);
   };
 
+  let expensesContent = <p> No expenses found. </p>;
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      ></ExpenseItem>
+    ))
+  };
+
   return (
     <div>
       <ExpensesFilter onFilter={onFilterHandler} />
       <Card className="expenses">
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          ></ExpenseItem>
-        ))}
+          {expensesContent}
       </Card>
     </div>
   );
